313f7c46f7f9975d08f4d42d3531871aae901971
An-Mir <anmir@inbox.lv>
2022-07-08 14:40:02 +0300

    Create DutyFree.java

---
+++ src/DutyFree/DutyFree.java
@@ -0,0 +1,39 @@
+package DutyFree;
+
+import java.util.Scanner;
+
+public class DutyFree {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String prices = input.nextLine();
+        String[] price = prices.split(" ");
+
+        double firstPrice = Double.parseDouble(price[0]);
+        double secondPrice = Double.parseDouble(price[1]);
+        double thirdPrice = Double.parseDouble(price[2]);
+        double fourthPrice = Double.parseDouble(price[3]);
+
+        if ((firstPrice * 1.1) >= 20) {
+            System.out.println("Back to the store");
+            if ((secondPrice * 1.1) >= 20) {
+                System.out.println("Back to the store");
+                if ((thirdPrice * 1.1) >= 20) {
+                    System.out.println("Back to the store");
+                    if ((fourthPrice * 1.1) >= 20) {
+                        System.out.println("Back to the store");
+                    } else {
+                        System.out.println("On to the terminal");
+                    }
+                } else {
+                    System.out.println("On to the terminal");
+                }
+            } else {
+                System.out.println("On to the terminal");
+            }
+        } else {
+            System.out.println("On to the terminal");
+        }
+    }
+}
